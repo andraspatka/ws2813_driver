cd4b169b032364fc92856b77a177cef926c39870
Patka Zsolt-Andras <andras.patka@gmail.com>
2019-12-09 20:17:06 +0200

    Dokumentáció kiegészítése szimulációs eredményekkel. Driver kód javítása

--- README.md
+++ README.md
@@ -18,12 +18,14 @@ Vivado design suite-ban:
  - IP Catalog
  - Keresőbe: "BRAM"
  - Block memory generator
- - Single port RAM opció
-   - Write/Read width: 24
-   - Write/Read depth: 100
+ - Single port ROM opció
+   - Width: 24
+   - Depth: 100
+ - Algorithm: Low Power
  - Other options:
    - Új *.coe file létrehozása
    - A következő példakód szerint kitölteni a coe file-t:
    - ```memory_initialization_radix=16;memory_initialization_vector=001,002,003,004,005,006,007,008,009,00A,00B,00C,00D,00E,00F,010; ```
    - "load init file" kipipálása
    - init file hozzáadása
+   - 
--- src/simulation/bram_simulation.vhd
+++ src/simulation/bram_simulation.vhd
@@ -32,17 +32,14 @@ component blk_mem_gen_0 is
   PORT (
     clka : IN STD_LOGIC;
     ena : IN STD_LOGIC;
-    wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
-    addra : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
-    dina : IN STD_LOGIC_VECTOR(23 DOWNTO 0);
+    addra : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
     douta : OUT STD_LOGIC_VECTOR(23 DOWNTO 0)
   );
 end component;
 
 signal ena : std_logic := '0';
-signal wea : std_logic_VECTOR(0 downto 0):="0";
-signal addra : std_logic_VECTOR(6 downto 0) := (others => '0');
-signal dina,douta : std_logic_VECTOR(23 downto 0) := (others => '0');
+signal addra : std_logic_VECTOR(3 downto 0) := (others => '0');
+signal douta : std_logic_VECTOR(23 downto 0) := (others => '0');
 signal clk : std_logic := '0';
 
 begin
@@ -51,11 +48,22 @@ BRAM_instance: blk_mem_gen_0
   port map (
     clka => clk,
     ena => ena,
-    wea => wea,
     addra => addra,
-    dina => dina,
     douta => douta
   );
+
+process
+begin
+    addra <= "0000";
+    wait for 5 ns;
+    ena <= '1';
+    for i in 0 to 14 loop
+        wait for 10 ns;
+        addra <= addra + "1";
+    end loop;
+    
+    wait;
+end process;
 
 clk_gen: process -- Generating a 100 MHz clock signal
 begin
@@ -63,19 +71,6 @@ begin
     wait for 5 ns;
     clk <='0';
     wait for 5 ns;     
-end process clk_gen; 
-
-process
-begin
-    wait for 5 ns;
-    addra <= "0000000";
-    for i in 0 to 15 loop
-        ena <= '1';
-        wea <= "0";
-        wait for 10 ns;
-        addra <= addra + "1";
-    end loop;
-    wait;
-end process;  
+end process clk_gen;   
 
 end Behavioral;
